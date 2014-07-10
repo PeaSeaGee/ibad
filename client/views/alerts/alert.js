290813652f86d1fbc1b4c26eea932dab60501308
Peter C. Gravelle <peter.gravelle@mongodb.com>
2014-07-10 10:49:20 -0400

    Put the alerts into their divs with string times

--- client/views/alerts/alert.js
+++ client/views/alerts/alert.js
@@ -2,3 +2,7 @@ Template.alert.alerts = function() {
 	answer = Alerts.find({},{sort:{whenAlert:-1}});
 	return answer;
 };
+
+Template.alert.strTime = function () {
+	return new Date(this.whenAlert).toString();
+}
